a493ec2e79ace93f8e1c97a652c4651a5e634a9a
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2026-04-24 18:33:36 +0200

    [feat] add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://imrohitsaini.in",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
